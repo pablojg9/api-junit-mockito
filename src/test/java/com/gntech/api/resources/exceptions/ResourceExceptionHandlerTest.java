0b19e060ae563bafdf37aad09a27c758c7f0d397
Pablo Junior <pablojuniorgn2@gmail.com>
2024-02-11 19:54:46 -0300

    feat: add tests to ResourceException and StandardError.  fix: changed the ResourceExceptionHandler response, it was null

---
+++ src/test/java/com/gntech/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,20 @@
+package com.gntech.api.resources.exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ResourceExceptionHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void objectNotFoundException() {
+    }
+
+    @Test
+    void dataIntegrityViolationExceptionHandler() {
+    }
+}
